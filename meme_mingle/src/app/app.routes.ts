e702c9a85a945ebb1a9e40d3f9fa8b3f9b7640ea
Yashhh230 <yp7202963@gmail.com>
2024-12-05 23:57:29 +0530

    sidebar done

--- meme_mingle/src/app/app.routes.ts
+++ meme_mingle/src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { AboutusComponent } from './layout/aboutus/aboutus.component';
-import { ContactComponent } from './layout/contact/contact.component';
 import { FaqComponent } from './layout/faq/faq.component';
 
 export const routes: Routes = [
-    {
+     {
         path: '',
         redirectTo: 'auth',
         pathMatch: 'full',
@@ -13,48 +12,36 @@ export const routes: Routes = [
       {
         path: 'auth',
         loadChildren: () => import('./auth/auth.routes').then((m) => m.routes),
+  },
+  {
+    path: 'main',
+    loadChildren: () => import('./main/main.routes').then((m) => m.main_routes),
       },
-  {
-        path: 'user-profile',
-        loadComponent: () =>
-          import('./user-profile/user-profile.component').then((m) => UserProfileComponent),
-  },
-  {
-    path: 'about',
-    loadComponent: () =>
-      import('./layout/aboutus/aboutus.component').then((m) => AboutusComponent),
-  },
-  
-    {
-      path: 'contact',
-      loadComponent: () =>
-        import('./layout/contact/contact.component').then((m) => m.ContactComponent),
-    },
-  {
-    path: 'FAQ',
-    loadComponent: () =>
-      import('./layout/faq/faq.component').then((m) => FaqComponent),
-  },
-  {
-    path: 'home',
-    loadComponent: () =>
-      import('./live-conversation/live-conversation.component').then((m) => m.LiveConversationComponent),
-  },
-      // {
-      //   path: 'virtual-character',
-      //   loadChildren: () =>
-      //     import('./virtual-character/virtual-charecter.routes').then(
-      //       (m) => m.routes
-      //   ),
-      // },
-      // {
-      //   path: 'live-conversation',
-      //   loadChildren: () =>
-      //     import('./live-conversation/live-conversation.routes').then(
-      //       (m) => m.routes
-      //     ),
-      // },
-
+  //     {
+  //       path: 'user-profile',
+  //       loadComponent: () =>
+  //         import('./user-profile/user-profile.component').then((m) => UserProfileComponent),
+  //    },
+  //    {
+  //   path: 'about',
+  //   loadComponent: () =>
+  //     import('./layout/aboutus/aboutus.component').then((m) => AboutusComponent),
+  //   },
+  //   {
+  //     path: 'contact',
+  //     loadComponent: () =>
+  //       import('./layout/contact/contact.component').then((m) => m.ContactComponent),
+  //   },
+  // {
+  //   path: 'FAQ',
+  //   loadComponent: () =>
+  //     import('./layout/faq/faq.component').then((m) => FaqComponent),
+  // },
+  // {
+  //   path: 'home',
+  //   loadComponent: () =>
+  //     import('./live-conversation/live-conversation.component').then((m) => m.LiveConversationComponent),
+  // },
       {
         path: '**',
         redirectTo: 'error',
--- meme_mingle/src/app/main/main.component.ts
+++ meme_mingle/src/app/main/main.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { SidebarComponent } from "../layout/sidebar/sidebar.component";
+import { HeaderComponent } from "../layout/header/header.component";
+import { SidebarService } from '../shared/service/sidebar.service';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule, RouterModule, SidebarComponent, HeaderComponent],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  sidebarVisible:boolean = true;
 
+  constructor(private sidebarService: SidebarService) {
+    this.sidebarService.getSidebarState().subscribe((visible:any) => {
+      this.sidebarVisible = visible;
+    });
+  }
 }
